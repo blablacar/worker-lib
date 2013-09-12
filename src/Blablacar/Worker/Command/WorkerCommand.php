dc2c8eb08ca6f1e13d1c298652f81de1314e24e9
Olivier Dolbeau <contact@odolbeau.fr>
2013-09-12 16:10:04 +0200

    Add missing use

--- src/Blablacar/Worker/Command/WorkerCommand.php
+++ src/Blablacar/Worker/Command/WorkerCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Blablacar\Worker\AMQP\Consumer\Context;
 
 abstract class WorkerCommand extends Command
 {
